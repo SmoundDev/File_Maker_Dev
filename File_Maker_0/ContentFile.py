1cf281eab690cbd68ed3c640f00d68630f2720be
EduardoTCW <133273187+EduardoTCW@users.noreply.github.com>
2023-06-16 16:55:23 -0600

    Add Logic Files for first Version

---
+++ File_Maker_0/ContentFile.py
@@ -0,0 +1,107 @@
+class File_Content:
+
+    def ReturnContentByExtension(nombre_archivo,_extension):
+
+        file_Content = ''
+
+        if (_extension == 'js'):
+            file_Content = '''
+            $(document).ready( function () {
+                getActualDate();
+            });
+
+            function getActualDate() {
+                // Date object
+                const date = new Date();
+
+                let currentDay = String(date.getDate()).padStart(2, '0');
+
+                let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+
+                let currentYear = date.getFullYear();
+
+                // we will display the date as DD/MM/YYYY 
+
+                let currentDate = `${currentDay}/${currentMonth}/${currentYear}`;
+
+                //console.log("The current date is " + currentDate);
+
+                $("#txtFecha_OC").val(currentDate)
+            }
+
+            '''
+        elif _extension == 'cs':
+            _nameSpace = input('Ingrese el espacio de nombres del controllador (Namespace): ')
+            
+            file_Content = (
+                    f'using Microsoft.AspNetCore.Mvc;\n'
+                    f'using {_nameSpace}.Models;\n'
+                    f'using System.Diagnostics;\n\n'
+                    f'namespace {_nameSpace}.Controllers\n'
+                    '{\n'
+                    f'    public class {nombre_archivo}Controller : Controller\n'
+                    '    {\n'
+                    f'        public IActionResult {nombre_archivo}()\n'
+                    '        {\n'
+                    # '            return View();\n'
+                    f'            return View(@"~\Views\{nombre_archivo}\{nombre_archivo}.cshtml");'
+                    '        }\n'
+                    '    }\n'
+                    '}\n'
+                )
+            nombre_archivo = f'{nombre_archivo}Controller'
+        elif _extension == 'cshtml':
+            
+            _title_view = input("Ingrese titulo de la vista (ViewBag.Title,h1): ")
+            _nameJs = input("Ingrese el nombre del Js de la vista: ")
+            
+            file_Content = (
+                '@{\n'
+                f'    ViewBag.Title = "{_title_view}";\n\n'
+                '    var sessionId = Context.Request.Cookies["SessionId"];\n'
+                '    var userRole = Context.Request.Cookies["SessionRol"];\n'
+                '    var userName = Context.Request.Cookies["SessionUName"];\n'
+                '    var userSurName = Context.Request.Cookies["SessionUSur"];\n'
+                '    var userlk = Context.Request.Cookies["Sessionlk"];\n\n'
+                '    var _model = new SessionModel();\n\n'
+                '    if (sessionId == null)\n'
+                '    {\n'
+                '        _model = new SessionModel();\n'
+                '    }\n'
+                '    else\n'
+                '    {\n'
+                '        _model = new SessionModel()\n'
+                '        {\n'
+                '            Apellido = userSurName,\n'
+                '            lk = userlk,\n'
+                '            Nombre = userName,\n'
+                '            rol = userRole,\n'
+                '        };\n'
+                '    }\n'
+                '}\n\n'
+                '@if (sessionId == null)\n'
+                '{\n'
+                '    <script src="~/lib/jquery/dist/jquery.min.js"></script>\n\n'
+                '    <script>\n\n'
+                '        $(document).ready(function () {\n'
+                '            var currentURL = $(location).attr(\'href\').split(\'/\');\n\n'
+                '            let returnURL = currentURL[0] + "//" + currentURL[2] + "/";\n'
+                '            window.location.replace(returnURL);\n'
+                '        })\n'
+                '    </script>\n'
+                '}\n'
+                'else\n'
+                '{\n\n'
+                '    <div class="Container-body">\n\n'
+                f'        <h1>{_title_view}</h1>\n'
+                '        <br />\n'
+                '        <div class="card-body">\n'
+                '        </div>\n'
+                '    </div>\n'
+                '}\n'
+                '    @section scripts{'
+                f'<script src="~/js/APP/{_nameJs}.js" crossorigin="anonymous"></script>'
+                '}'
+            )
+
+        return file_Content, nombre_archivo
---
+++ File_Maker_0/FileMaker.py
@@ -0,0 +1,55 @@
+import os
+from ContentFile import File_Content as fc
+
+
+def crear_archivo(ruta_destino, nombre_archivo, extension_archivo):
+
+    _fileContent, _nombre_archivo = fc.ReturnContentByExtension(
+        nombre_archivo, extension_archivo)
+
+    extension_archivo = '.'+extension_archivo
+    ruta_completa = os.path.join(
+        ruta_destino, _nombre_archivo+extension_archivo)
+    
+    newFolder = ''
+    if '\\' in _nombre_archivo:
+        newFolder = _nombre_archivo.split('\\');
+        if not os.path.exists(os.path.join(ruta_destino,newFolder[0])):
+            os.makedirs(os.path.join(ruta_destino,newFolder[0]))
+
+    with open(ruta_completa, 'w') as archivo:
+        archivo.write(_fileContent)
+        # archivo.write('$(document).ready(function (e){\n});\n')
+    print(
+        f'Se ha creado el archivo {_nombre_archivo} en la ruta {ruta_destino}')
+
+
+def main():
+    ruta_destino = input('Ingrese la ruta destino: ')
+
+    while True:
+        nombre_archivo = input('Ingrese el nombre de archivo: ')
+        extencion_archivo = input(
+            'Ingrese la extension del archivo (sin punto): ')
+
+        if '.' in extencion_archivo:
+            extencion_archivo = extencion_archivo.replace('.', '')
+
+        crear_archivo(ruta_destino, nombre_archivo, extencion_archivo)
+        respuesta = input('¿Desea crear otro archivo? (s/n): ')
+        if respuesta.lower() != 's':
+            break
+
+    while True:
+        respuesta = input(
+            '¿Desea ingresar una nueva ruta destino? (presione "ctrl + m" para continuar): ')
+        if respuesta == '\x0d':
+            ruta_destino = input('Ingrese la nueva ruta destino: ')
+            nombre_archivo = input('Ingrese el nombre de archivo: ')
+            crear_archivo(ruta_destino, nombre_archivo + '.js')
+        else:
+            break
+
+
+if __name__ == '__main__':
+    main()
